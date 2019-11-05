c75fced799f3847c959f2f67e461ba97d9c6b751
tranminhhi2000 <tranminhhi2732000@gmail.com>
2019-11-05 14:36:08 +0700

    5/11/2019

---
+++ OOPExercise/Oop2.3/MyCircle.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopEx
+{
+    class MyCircle
+    {
+        private MyPoint center;
+        private int radius = 1;
+        public MyCircle()
+        {
+            center = new MyPoint(0, 0);
+            radius = 1;
+        }
+        public MyCircle(int x, int y, int radius)
+        {
+            center = new MyPoint(x, y);
+            this.radius = radius;
+        }
+        public MyCircle(MyPoint center, int radius)
+        {
+            this.center = center;
+            this.radius = radius;
+        }
+        public int getRadius()
+        {
+            return radius;
+        }
+        public void setRadius(int radius)
+        {
+            this.radius = radius;
+        }
+        public MyPoint getCenter()
+        {
+            return center;
+        }
+        public void setCenter(MyPoint center)
+        {
+            this.center = center;
+        }
+        public int getCenterX()
+        {
+            return center.getX();
+        }
+        public void setCenterX(int x)
+        {
+            center.setX(x);
+        }
+        public int getCenterY()
+        {
+            return center.getY();
+        }
+        public void setCenterY(int y)
+        {
+            center.setY(y);
+        }
+        public int[] getCenterXY()
+        {
+            int[] xy = new int[2];
+            xy[0] = center.getX();
+            xy[1] = center.getY();
+            return xy;
+        }
+        public void setCenterXY(int x, int y)
+        {
+            center.setX(x);
+            center.setY(y);
+        }
+        public override string ToString()
+        {
+            return string.Format("My circle[radius = {0},center = {1}]", getRadius(), center.ToString());
+            //return base.ToString();
+        }
+        public double getArea()
+        {
+            return radius * radius * Math.PI;
+        }
+        public double getCircumference()
+        {
+            return 2 * radius * Math.PI;
+        }
+        public double distance(MyCircle another)
+        {
+            return center.distance(another.center);
+        }
+    }
+}
---
+++ OOPExercise/Oop2.3/MyPoint.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopEx
+{
+    class MyPoint
+    {
+        private int x = 0;
+        private int y = 0;
+
+        public MyPoint()
+        {
+            x = 0;
+            y = 0;
+        }
+        public MyPoint(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public int getX()
+        {
+            return x;
+        }
+        public void setX(int x)
+        {
+            this.x = x;
+        }
+        public int getY()
+        {
+            return y;
+        }
+        public void setY(int y)
+        {
+            this.y = y;
+        }
+        public int[] getXY()
+        {
+            int[] xy = new int[2];
+            xy[0] = this.x;
+            xy[1] = this.y;
+            return xy;
+        }
+        public void setXY(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public override string ToString()
+        {
+            return string.Format("({0},{1})", getX(), getY());
+            //return base.ToString();
+        }
+        public double distance(int x, int y)
+        {
+            int xDiff = this.x - x;
+            int yDiff = this.y - y;
+            return Math.Sqrt(xDiff * xDiff + yDiff * yDiff);
+        }
+        public double distance(MyPoint another)
+        {
+            int xDiff = this.x - another.x;
+            int yDiff = this.y - another.y;
+            return Math.Sqrt(xDiff * xDiff + yDiff * yDiff);
+        }
+        public double distance()
+        {
+
+            return distance(0, 0);
+        }
+    }
+}
